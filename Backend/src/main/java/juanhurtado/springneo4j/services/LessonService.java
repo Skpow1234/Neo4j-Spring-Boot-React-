45924f37ae9f7723d2f54fdcb3d019b0f6de8b22
Juan Hurtado <jfhvjfhv0015@gmail.com>
2023-12-18 14:04:57 -0500

    Mejorar frontend

---
+++ Backend/src/main/java/juanhurtado/springneo4j/services/LessonService.java
@@ -0,0 +1,20 @@
+package juanhurtado.springneo4j.services;
+
+import juanhurtado.springneo4j.models.Lesson;
+import juanhurtado.springneo4j.repositories.LessonRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LessonService {
+    private final LessonRepository lessonRepository;
+
+    public LessonService(LessonRepository lessonRepository) {
+        this.lessonRepository = lessonRepository;
+    }
+
+    public List<Lesson> getAllLessonsByCourseIdentifier(String identifier) {
+        return lessonRepository.findLessonsByCourseIdentifier(identifier);
+    }
+}
---
+++ Backend/src/main/java/juanhurtado/springneo4j/services/NeoUserDetailsService.java
@@ -0,0 +1,24 @@
+package juanhurtado.springneo4j.services;
+
+import juanhurtado.springneo4j.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NeoUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public NeoUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found" + username));
+    }
+}
